3cf4e54e7ca816d3290d14d5ad163e3aabd46f89
mhkapr <mh.kalatearabi@gmail.com>
2025-11-15 12:21:29 +0330

    add MessageDTO to repositories

--- interfaces/tg_repository.py
+++ interfaces/tg_repository.py
@@ -2,29 +2,31 @@ from abc import ABC, abstractmethod
 from typing import List, Union
 import uuid
 
+from DTOs.repo_dtos import MessageDTO
+
 
 class BaseTgRepo(ABC):
 
     @abstractmethod
-    def create(self, data: dict) -> dict:
+    def create(self, data: MessageDTO) -> MessageDTO:
         pass
 
     @abstractmethod
-    def update(self, data: dict) -> dict:
+    def update(self, data: MessageDTO) -> MessageDTO:
         pass
 
     @abstractmethod
-    def delete(self, tg_id: int) -> None:
+    def delete(self, tg_id: Union[int, uuid.UUID]) -> None:
         pass
 
     @abstractmethod
-    def store(self, msg_object):
+    def store(self, msg_object: MessageDTO) -> MessageDTO:
         pass
 
     @abstractmethod
-    def load(self, id):
+    def load(self, id: Union[int, uuid.UUID]) -> MessageDTO:
         pass
 
     @abstractmethod
-    def load_all(self, ):
+    def load_all(self) -> List[MessageDTO]:
         pass
--- interfaces/truth_repository.py
+++ interfaces/truth_repository.py
@@ -1,28 +1,29 @@
 from abc import ABC, abstractmethod
 from typing import List, Union
 import uuid
+from DTOs.repo_dtos import MessageDTO
 
 
 class BaseTruthRepo(ABC):
     
     @abstractmethod
-    def get_by_id(id: Union[int, uuid.UUID]) -> dict:
+    def get_by_id(id: Union[int, uuid.UUID]) -> MessageDTO:
         pass
 
     @abstractmethod
-    def filter(**fields) -> List[dict]:
+    def filter(**fields) -> List[MessageDTO]:
         pass
 
     @abstractmethod
-    def all() -> List[dict]:
+    def all() -> List[MessageDTO]:
         pass
 
     @abstractmethod
-    def create(data: dict) -> dict:
+    def create(data: MessageDTO) -> MessageDTO:
         pass
 
     @abstractmethod
-    def update(data: dict) -> dict:
+    def update(data: MessageDTO) -> MessageDTO:
         pass
 
     @abstractmethod
